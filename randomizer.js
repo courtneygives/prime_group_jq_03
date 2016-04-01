f86fe2df8e03dbe7d66d1f8e31eaf3f2ee1604a3
Courtney Ives <cgives@me.com>
2016-03-31 22:39:15 -0500

    #1 Added input field which can replace names array

--- randomizer.js
+++ randomizer.js
@@ -2,7 +2,14 @@ $(function() {
 
   //------------------ permanent array of individuals ----------------------//
 
-  var permanentNames = ["Tuttle", "Chris R", "Jessica", "Jennifer", "Taylor", "Erika", "Sasha", "Amy", "Cari", "Oliver", "Travis", "Courtney", "Russ", "Libby", "Lisa", "Max", "Kyle", "Suzanna", "Peter"]
+  var permanentNames = function() {
+    var roster = $(".groupMaker textarea").val().split(",");
+    if (roster.length > 1){
+      return roster;
+    } else {
+      return ["Tuttle", "Chris R", "Jessica", "Jennifer", "Taylor", "Erika", "Sasha", "Amy", "Cari", "Oliver", "Travis", "Courtney", "Russ", "Libby", "Lisa", "Max", "Kyle", "Suzanna", "Peter"];
+    }
+  }
 
   //--------------------------- team name generator -----------------------//
   //--------------------------- number buttons ---------------------------//
@@ -11,31 +18,37 @@ $(function() {
 
   var permanentAdjs = ["Funky", "Raging", "Surly", "Plain", "Purple", "Dangerous", "Unsinkable", "Wounded", "Wooden", "Laminated", "Jolly", "Four-Legged", "Peachy", "Gelatinous", "Possibly-Illegal", "Hydrophobic", "Powdery", "Troubled", "Dreamy", "Ludicrous", "Authoritative", "Definitely-Trustworthy", "Heroic", "Righteous", "Adjustable", "Disposable", "Super Fast", "Rambunctious"]
 
-  var permanentNouns = ["Donkies", "Hors d'oeuvres", "Bananna Sandwiches", "Troupers", "Ents", "Pterodactyls", "Ponies", "Narwals", "Tauntauns", "Statues", "Squishy Cubes", "Cylinders", "Grassy Knolls", "Hot Dishes", "Lake-Dwellers", "Jam-bro-rees", "Blades", "Time-Travelers", "Astronauts"]
+  var permanentNouns = ["Donkeys", "Hors d'oeuvres", "Bananna Sandwiches", "Troupers", "Ents", "Pterodactyls", "Ponies", "Narwhals", "Tauntauns", "Statues", "Squishy Cubes", "Cylinders", "Grassy Knolls", "Hot Dishes", "Lake-Dwellers", "Jam-bro-rees", "Blades", "Time-Travelers", "Astronauts"]
 
   var teams=[[]];    //declare two-dimensional array
 
   var numTeams= 0;
 
   $(".number").click(function(){
+//    clearScreen();
     numTeams = $(this).children().attr("alt");
     console.log(numTeams)
-
   });
 
   //generate button
   $("#generateButton").click(function(){
+    // clearScreen();
     if (numTeams == 0) {
       alert("Please select number of groups!");
     }else {
       generateTeams(numTeams);
       generateTeamNames(numTeams);
       writeGroups(numTeams);
-
-      console.log(teams);
-      console.log(numTeams)
     }
   });
+  // 
+  // function clearScreen (){
+  //  $(".groups").empty();
+  //  for (i=0; i<numTeams; i++){
+  //    teams[i].shift();
+  //  }
+  // }
+
 
 function writeGroups (numTeams){
   for (i = 0; i < numTeams; i++){
@@ -46,36 +59,14 @@ function writeGroups (numTeams){
     teamDisplay += "</ul>";
     $(".groups").append(teamDisplay);
   }
-
 }
 
-
-  //--------------------------- shuffler ---------------------------//
-
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
 
 
   //--------------------------- generate teams ---------------------------//
 
   function generateTeams (numTeams) {    //// takes shuffled array
-    var randNames=shuffle(permanentNames);
+    var randNames=shuffle(permanentNames());
     var groupI = 0;
     console.log(numTeams)
 
@@ -101,5 +92,27 @@ function writeGroups (numTeams){
       teams[it].unshift(randAdjs[it] + " " + randNouns[it]);
     }
   }
+
+  //--------------------------- shuffler ---------------------------//
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
 
 });
